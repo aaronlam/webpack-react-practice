56428ce1a3a9586acf14d37c124910792716d47d
aaronlam <a@aaronlam.xyz>
2020-10-20 22:12:47 +0800

    update HMR

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import React, { Component } from "react";
 import ReactDom from "react-dom";
 import App from "./App";
 
-// webpack热模块替换设置
+ReactDom.render(<App />, document.getElementById("app"));
+
+// webpack热模块替换
 if (module.hot) {
-  module.hot.accept();
+  // 接受更新
+  module.hot.accept("./App.js", function () {
+    // 重新render
+    const NextApp = require("./App");
+    ReactDom.render(<NextApp />, document.getElementById("app"));
+  });
 }
 
-class Index extends Component {
-  render() {
-    return <App />;
-  }
-}
-
-ReactDom.render(<Index />, document.getElementById("app"));
+// if (module.hot) {
+//   module.hot.accept();
+// }
